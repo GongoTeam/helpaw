c20e1b57717d904a365a2d6dcf0c36add791287f
Роман Сікорський <romansikorskiy@MacBook-Air-Roman.local>
2025-04-10 19:44:30 +0300

    Initial commit with folder structure and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CreateListing from "./pages/CreateListing";
+import AnimalCard from "./pages/AnimalCard";
+import Search from "./pages/Search";
+import Shelter from "./pages/Shelter";
+import RequestForm from "./pages/RequestForm";
+import FoundAnimal from "./pages/FoundAnimal";
+import News from "./pages/News";
+import Donate from "./pages/Donate";
+import Raffles from "./pages/Raffles";
+import ShelterRatings from "./pages/ShelterRatings";
+import PetStories from "./pages/PetStories";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/animal/:id" element={<AnimalCard />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/shelter/:id" element={<Shelter />} />
+        <Route path="/request" element={<RequestForm />} />
+        <Route path="/found" element={<FoundAnimal />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/raffles" element={<Raffles />} />
+        <Route path="/ratings" element={<ShelterRatings />} />
+        <Route path="/stories" element={<PetStories />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
